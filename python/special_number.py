731fc85d1137d7a42023abb484fd4193e1da52ac
Dmitry Stetyukha <s.dmitriy@webpractik.ru>
2023-09-01 16:33:55 +0300

    fix returning data

--- python/special_number.py
+++ python/special_number.py
@@ -4,8 +4,10 @@ import re
 def get_good_numbers(unique_number: str):
     pattern = r"(\d{2,4}\\{1}\d{2,5})"
     numbers = re.findall(pattern, unique_number)
-
+    good_numbers = []
     for number in numbers:
         parts = number.split("\\")
         good_number = parts[0].zfill(4) + "\\" + parts[1].zfill(5)
-        return good_number
+        good_numbers.append(good_number)
+
+	return good_numbers
